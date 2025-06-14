b16160352be56f21444029fdda3c667919f5e76c
18ANUJSHARMA <anuj1642004@gmail.com>
2025-06-14 23:56:52 +0530

    new day sith started php learning.

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+$str = "i love flower .";
+echo strlen($str). "<br>";// length of string incluing spaces
+echo str_word_count($str). "<br>";// count total number of words 
+
+var_dump($str); // print the type of data with it's length 
+
+
+
+?>
